8855e688e2f2984b99b31bde1c88de7cf6bafcf1
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-11 16:23:28 +0200

    Login router made

---
+++ src/pages/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    home
+  </template>
+  
+  <script>
+  import {defineComponent} from 'vue'
+  
+  export default defineComponent({
+    name: "Home"
+  })
+  </script>
+  
+  
+  
+  <style scoped>
+  
+  </style>
