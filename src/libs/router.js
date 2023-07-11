15e34f09d714da6f2f7a164ceda0d1450e06bdfb
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-11 16:43:13 +0200

    message page

--- src/libs/router.js
+++ src/libs/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
+import Message from "../pages/Message";
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
-  
+  { path: '/message', component: Message }
+
 ]
 
 const router = createRouter({
