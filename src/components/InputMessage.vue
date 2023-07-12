aa21e607054070aee590aec20c96ce22a2f714ff
Mirziano <Spagnolo_mg@outlook.com>
2023-07-12 09:39:45 +0200

    premier jet input msg

---
+++ src/components/InputMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- <v-container>
+        Possibilité de recupérer le message  via : {{ text }}
+    </v-container> -->
+
+    <v-container>
+    <v-textarea
+      v-model="text"
+    ></v-textarea>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            text:'',
+        };
+    },
+};
+</script>
+
+
+
+<style scoped>
+    
+</style>
---
+++ src/pages/TestInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+<v-app-bar>
+    <router-view></router-view>
+</v-app-bar>
+    
+ 
+
+<v-footer>
+    <InputMessage />
+</v-footer>
+
+</v-app>
+</template>
+
+<script>
+import InputMessage from "./components/InputMessage.vue"
+
+
+export default {
+    components: {InputMessage}
+}
+
+</script>
+
+<style scoped>
+
+</style>
