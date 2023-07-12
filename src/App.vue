9613d64422610551080742f4f6b4694ec582bae4
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-12 08:50:53 +0200

    | leave

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-     <router-link to="/">Home</router-link> |
+     <router-link to="/">Home</router-link>
     <router-link to="/Login">Login</router-link>
     <router-link to="/Messages">Messages</router-link>
     <router-view></router-view>
