42ac5975854656c850dc9afa9adc9308fb66a717
Yasm-ina <boumarafyasmina1991@gmail.com>
2023-07-13 11:45:20 +0200

    userProfile created  fix: #24

--- src/components/ChatBanner.vue
+++ src/components/ChatBanner.vue
@@ -1,7 +1,7 @@
 <template>
    <v-container class="bg-cyan-lighten-4">
     <v-row >
-    <v-col ><h3 class="text-center pt-2">Room name {{ roomname }}</h3></v-col>
+    <v-col ><h3 class="text-center pt-2">Room name {{ roomName }}</h3></v-col>
     <v-col cols="1" class="me-6"><v-avatar icon="$vuetify"> </v-avatar></v-col>    
     </v-row>
 </v-container>
