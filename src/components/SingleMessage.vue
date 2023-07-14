1e079aad66b08231f1943b9bf69d4ab5454fbc6f
Daniel Sajdak <danielsajdak65@gmail.com>
2023-07-14 16:44:44 +0200

    fix

--- src/components/SingleMessage.vue
+++ src/components/SingleMessage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="sender == 'Tom'  ? 'd-flex justify-end' : 'd-flex justify-start'">
+    <div :class="sender == ''  ? 'd-flex justify-end' : 'd-flex justify-start'">
         <div class="border rounded-xl rounded-be-0 messageFitContent" >
             <h5 class="pa-3">
                 Sender: {{ sender }}
@@ -33,7 +33,7 @@
             },
             sender: {
                 type: String,
-                default: 'Tom ',
+                default: '',
                 requaired: false
             },
             ts: {
