eafe75515e9176b14831062e34af55d4b7bc537f
Mirziano <Spagnolo_mg@outlook.com>
2023-07-14 14:15:16 +0200

    improvement - emoji picker still not workin but visible

---
+++ src/components/Emoji.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <VuemojiPicker @emojiClick="handleEmojiClick" />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { VuemojiPicker, EmojiClickEventDetail } from 'vuemoji-picker'
+  
+  export default{
+    
+    components: {
+      VuemojiPicker
+    },
+    methods: {
+      handleEmojiClick(detail: EmojiClickEventDetail) {},
+    
+      openEmoji(){
+        this.visible = true;
+      },
+
+    }
+  }
+  </script>
--- src/components/InputMessage.vue
+++ src/components/InputMessage.vue
@@ -1,17 +1,19 @@
 <template>
-    
     <v-container class="pa-10 bg-blue">
         <v-row>
             <v-col cols="11">
                 <v-textarea v-model="msgText" variant="solo" auto-grow bg-color="cyan-lighten-4">
-
+                  
                 </v-textarea>
             </v-col>
             <v-col cols="1">
-                <v-btn class="pa-3 rainbow" rounded="xl" icon="mdi-emoticon-happy-outline"></v-btn>
+                
+                  <v-btn @click="show = !show">
+                    <Emoji v-show="show"/>
+                </v-btn>
                 <br>
                 <br>
-                <v-btn class="pa-3" icon="mdi-send-circle" @click="sendMessage()" rounded="xl" color="teal-accent-3"></v-btn>
+                <v-btn class="pa-3" icon="mdi-send" @click="sendMessage()" rounded="xl" color="teal-accent-3" v-show="!show"></v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -19,30 +21,34 @@
 
 
 <script>
+import Emoji from './Emoji.vue';
 import '@mdi/font/css/materialdesignicons.css'
+
 export default {
     data() {
         return {
             msgText: '',
+            show:false
+
         };
-        
+
     },
-    //création de la methode qui va servir à receuillir les informations tapé stocké dans msgText qui correspond à la valeur de l'input
+    components: { Emoji },
+    //receuillir les informations stocké dans msgText qui correspond à la valeur de l'input
     methods: {
-            sendMessage(){
-                this.$emit('sendme', this.msgText)
+        sendMessage() {
+            this.$emit('sendme', this.msgText)
+            this.msgText = ""
         },
-}
+        
+    }
 }
 </script>
 
 
 
 <style scoped>
-.rainbow{
+.rainbow {
     background: linear-gradient(25deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);
 }
-
-
-
 </style>
