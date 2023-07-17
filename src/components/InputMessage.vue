cf3d0b61033a8b69de2c109d1ce3cef56bfc1222
Mirziano <Spagnolo_mg@outlook.com>
2023-07-17 10:00:02 +0200

    dernier test d'ajout emoji bug car retour empty string

--- src/components/InputMessage.vue
+++ src/components/InputMessage.vue
@@ -2,18 +2,13 @@
     <v-container class="pa-10 bg-blue">
         <v-row>
             <v-col cols="11">
-                <v-textarea v-model="msgText" variant="solo" auto-grow bg-color="cyan-lighten-4">
-                  
-                </v-textarea>
+                <!-- <v-textarea v-model="msgText" variant="solo" auto-grow bg-color="cyan-lighten-4">
+
+                </v-textarea> -->
+                <EmojiPicker v-model="msgText" picker-type="textarea" style="background-color: rgb(42, 125, 198);" @select="onSelectEmoji" />
             </v-col>
             <v-col cols="1">
-                
-                  <v-btn @click="show = !show">
-                    <Emoji v-show="show"/>
-                </v-btn>
-                <br>
-                <br>
-                <v-btn class="pa-3" icon="mdi-send" @click="sendMessage()" rounded="xl" color="teal-accent-3" v-show="!show"></v-btn>
+                <v-btn class="pa-3" icon="mdi-send" @click="sendMessage()" rounded="xl" color="teal-accent-3"></v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -21,26 +16,34 @@
 
 
 <script>
-import Emoji from './Emoji.vue';
+import EmojiPicker from "vue3-emoji-picker";
+import "../../node_modules/vue3-emoji-picker/dist/style.css";
 import '@mdi/font/css/materialdesignicons.css'
 
 export default {
     data() {
         return {
             msgText: '',
-            show:false
-
         };
-
+        
     },
-    components: { Emoji },
-    //receuillir les informations stocké dans msgText qui correspond à la valeur de l'input
+    components: {
+        EmojiPicker: EmojiPicker,
+    },
     methods: {
-        sendMessage() {
-            this.$emit('sendme', this.msgText)
-            this.msgText = ""
+        onSelectEmoji(emoji) {
+            this.msgText += emoji.i;
         },
         
+        showEmoji(emoji) {
+            alert(`emoji ${emoji.i} selected, check console for details`);
+        },
+        //receuillir les informations stocké dans msgText qui correspond à la valeur de l'input
+        sendMessage() {
+            this.$emit('sendme', this.msgText)
+            this.msgText = "",
+            this.emoji =""
+        }
     }
 }
 </script>
