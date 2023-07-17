c89c77d2c6776160083eee5003d5e04fe72c51c9
Yasm-ina <boumarafyasmina1991@gmail.com>
2023-07-17 14:01:08 +0200

    AvatarCodeDone fix:  #44

--- src/components/AvatarComponent.vue
+++ src/components/AvatarComponent.vue
@@ -1,26 +1,11 @@
 <template>
-  <span :style="avatarStyle" v-bind="$attrs"/>
+  <span :style="avatarStyle" v-bind="$attrs" />
 </template>
 
 <script>
-
 export default {
-  // props: {
-  //   avatarStyle:{
 
-  //   },
-  //   currentGroup:{
-  //     type: [Number,String],
-  //     default: null,
-  //   },
-  //   posX:{
-  //     type: [Number,String],
-  //     default: null,
-  //   },
-  //   posY:{
-  //     type: [Number,String],
-  //     default: null,
-  //   },
+  props: {
     size: {
       type: [Number, String],
       default: 48,
@@ -42,49 +27,66 @@ export default {
       validator(value) {
         return value >= 1 && value <= 2;
       },
-    },
-  }
-
-  computed: () => {
-    this.currentGroup = this.GROUPS[this.group - 1]
-    this.posX = (this.toyNumber - 1) % this.currentGroup.value.cols
-    this.posY = Math.floor((this.toyNumber - 1) / this.currentGroup.value.cols)
-    this.avatarStyle = {
-      display: "inline-block",
-      width: this.size + "px",
-      height: this.size + "px",
-      backgroundImage: this.currentGroup.value.img,
-      backgroundRepeat: "no-repeat",
-      backgroundSize:
-          this.size * this.currentGroup.value.cols +
-          "px " +
-          this.size * this.currentGroup.value.rows +
-          "px",
-      margin: "8px",
-      borderRadius: this.rounded + "px",
-      backgroundPositionX: `-${this.posX.value * this.size}px`,
-      backgroundPositionY: `-${this.posY.value * this.size}px`,
     }
-  };
-  data: () => {
-    return {
-      
+  },
 
+  data() {
+    return {
       GROUPS: [
         {
           rows: 3,
           cols: 6,
           img: "url('https://user-images.githubusercontent.com/58827166/184355357-0b278997-c163-45cf-a575-19f532b07864.jpg')",
         },
-        
         {
           rows: 3,
           cols: 6,
           img: "url('https://user-images.githubusercontent.com/58827166/184355612-a8b12e00-a815-4456-8892-30836b4d1c2c.jpg')",
         },
-      ],
-      
+      ]
+    }
+  },
+
+  computed: {
+    currentGroup: {
+      get() {
+        return this.GROUPS[this.group.length - 1];
+      }
+    },
+    posX: {
+      get() {
+        return (this.toyNumber - 1) % this.currentGroup.cols
+      },
+    },
+
+    posY: {
+      get() {
+        return Math.floor((this.toyNumber - 1) / this.currentGroup.cols)
+      }
+    },
+
+    avatarStyle: {
+      get() {
+        return {
+          display: "inline-block",
+          width: this.size + "px",
+          height: this.size + "px",
+          backgroundImage: this.currentGroup.img,
+          backgroundRepeat: "no-repeat",
+          backgroundSize:
+            this.size * this.currentGroup.cols +
+            "px " +
+            this.size * this.currentGroup.rows +
+            "px",
+          margin: "8px",
+          borderRadius: this.rounded + "px",
+          backgroundPositionX: `-${this.posX * this.size}px`,
+          backgroundPositionY: `-${this.posY * this.size}px`
+        }
+      }
     }
   }
+}
+
 
 </script>
--- src/components/userProfil.vue
+++ src/components/userProfil.vue
@@ -1,12 +1,13 @@
 <template>
     <v-container class="bg-cyan-lighten-4">
-        <v-row cols=12 class="ma-3">
-            <v-col justify="start" cols="4">
-                <AvatarComponent v-model="$attrs" toyNumber:='1' :size="70" rounded="28" group="1"
-                    id="avatarShadow" />
+        <v-row class="pa-3 ma-3">
+            <v-col justify="start">
+                <AvatarComponent  :toyNumber='1' :size="60" rounded="50" group="1" id="avatarShadow" />
             </v-col>
-            <v-col cols="8" justify="end">
-                User information
+            <v-col justify="end">
+                <h3 text-align="center" class="ma-7">
+                    User information
+                </h3>
             </v-col>
         </v-row>
     </v-container>
@@ -14,25 +15,21 @@
 <script>
 
 import AvatarComponent from './AvatarComponent.vue'
-
-
 export default {
     components: { AvatarComponent },
     props: {
-    },
-    data: () => {
-        return {
- 
-        }
-    },
-    methods: {
-
-
+        data: () => {
+            return {
+                
+            }
+        },
+        methods: {
+        },
     }
 };
 </script>
 <style>
 #avatarShadow {
-    box-shadow: 10px 10px 12px 1px #0f1fff8b;
+    box-shadow: 10px 10px 12px 1px #6c35ba8b;
 }
 </style>
