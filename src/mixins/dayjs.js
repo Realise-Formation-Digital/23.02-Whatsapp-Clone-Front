4496840a17b5c288990a9de7fe228451b713a4ba
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-18 13:42:14 +0200

    comments del

--- src/mixins/dayjs.js
+++ src/mixins/dayjs.js
@@ -1,21 +1,5 @@
 import dayjs from 'dayjs';
 
-// format otherDay    mettre dans votre code import dayjss from..., const otherDay = 'date en fonction du format ci dessous, voir plus bas les ex', const formattedOtherDay = dayjs(otherDay).formatOtherDay();
-/*
-function formatOtherDay(date) {
-  return dayjs(date).format('DD-MM-YYYY:HH:mm:ss-Z:+02:00','DD-MM-YYYY','DD-MM-YYYY:HH:mm:ss','DD-MM-YYYY:HH:mm','ddd, DD-MM-YYYY:HH:mm:ss');
-}
-
-// add the format function to the prototype Day.js
-dayjs.extend((_, Dayjs) => {
-  Dayjs.prototype.formatOtherDay = function () {
-    return formatOtherDay(this.$d);
-  };
-});
-*/
-
-
-
 let dayjsMixins = {
   methods: {
     //taper dayjs() pour la date d'auj, taper dayjs.local('fr-CH') pour la date local
