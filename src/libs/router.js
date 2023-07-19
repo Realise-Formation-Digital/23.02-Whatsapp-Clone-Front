cf93392f2f050dec5a031648332d73143e1059cb
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-19 12:24:42 +0200

    ok

--- src/libs/router.js
+++ src/libs/router.js
@@ -2,15 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
 import Messages from "../pages/Messages.vue";
+import {chatStore} from "../store/store.js"
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/messages', component: Messages }
 ]
-  
+
 const router = createRouter({
   routes: routes,
   history: createWebHistory()
+})
+router.beforeEach(async (to, from) => {
+  if (
+    username == ''
+  ) {console.log ('nope')}
 })
 
 export default router
