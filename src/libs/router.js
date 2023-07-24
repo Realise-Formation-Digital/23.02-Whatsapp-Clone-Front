75e5b3f654401b58e9cb9e893c514df67c226683
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-24 11:23:53 +0200

    Supposed to be that but it doesn't work

--- src/libs/router.js
+++ src/libs/router.js
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from '../store/store.js';
+import { createPinia } from 'pinia.js';
+import { createApp } from 'vue';
+import App from 'App.vue';
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
 import Messages from "../pages/Messages.vue";
 import {chatStore} from "../store/store.js"
+
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
@@ -13,10 +18,29 @@ const router = createRouter({
   routes: routes,
   history: createWebHistory()
 })
-router.beforeEach(async (to, from) => {
-  if (
-    username == ''
-  ) {console.log ('nope')}
+
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+
+// ✅ works because the pinia instance is now active
+const userStore = useUserStore()
+
+const store = useStore()
+
+router.beforeEach((to, from, next) => {
+  // we wanted to use the store here
+  if (store.isLoggedIn) next()
+  else next('/login')
 })
+
+router.beforeEach((to) => {
+  // ✅ This will work because the router starts its navigation after
+  // the router is installed and pinia will be installed too
+  const store = useStore()
+
+  if (to.meta.requiresAuth && !store.isLoggedIn) return '/login'
+})
+
 
 export default router
