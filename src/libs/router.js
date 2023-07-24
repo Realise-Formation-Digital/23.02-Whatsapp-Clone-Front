d7993a8fc13cf9fb9e20be4a4c75a0a4ebf534a8
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-07-24 15:30:11 +0200

    comments leave

--- src/libs/router.js
+++ src/libs/router.js
@@ -18,28 +18,4 @@ const router = createRouter({
   history: createWebHistory()
 })
 
-// const pinia = createPinia()
-// const app = createApp(App)
-// app.use(pinia)
-
-// ✅ works because the pinia instance is now active
-// const userStore = createPinia()
-
-// const store = chatStore()
-
-// router.beforeEach((to, from, next) => {
-//   // we wanted to use the store here
-//   if (userStore.isLoggedIn) next()
-//   else next('/login')
-// })
-
-// router.beforeEach((to) => {
-//   // ✅ This will work because the router starts its navigation after
-//   // the router is installed and pinia will be installed too
-//   const store = chatStore()
-
-//   if (to.meta.requiresAuth && !store.isLoggedIn) return '/login'
-// })
-
-
 export default router
