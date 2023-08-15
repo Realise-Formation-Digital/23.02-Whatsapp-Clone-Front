bb21102cdcb11b4161c5d54a259c0f7234431ed0
Marco Tribuzio <marco.tribuz@gmail.com>
2023-08-15 16:22:36 +0200

    WIP avatar style

--- src/components/AvatarComponent.vue
+++ src/components/AvatarComponent.vue
@@ -7,91 +7,75 @@ export default {
 
   props: {
     size: {
-      type: String,
-      default: "",
-      required: true
+      type: [Number, String],
+      default: 48
     },
     rounded: {
-      type: String,
-      default: "",
-      required: true
+      type: [Number, String],
+      default: 0,
     },
     toyNumber: {
       type: [Number, String],
-      default: "",
       default: 1,
       validator(value) {
         return value >= 1 && value <= 18;
       },
-
-      group: {
-        type: [Number, String],
-        default: 1,
-        validator(value) {
-          return value >= 1 && value <= 2;
-        },
+    },
+    group: {
+      type: [Number, String],
+      default: 1,
+      validator(value) {
+        return value >= 1 && value <= 2;
       },
     },
-
-    data() {
-      return {
-        groups: [
-          {
-            rows: 3,
-            cols: 6,
-            //  allowedExtensions : /(\.jpg)$/i,
-            img: "url('https://user-images.githubusercontent.com/58827166/184355357-0b278997-c163-45cf-a575-19f532b07864.jpg')",
-
-
-          },
-
-          {
-            rows: 3,
-            cols: 6,
-            img: "url('https://user-images.githubusercontent.com/58827166/184355612-a8b12e00-a815-4456-8892-30836b4d1c2c.jpg')",
-          },
-          console.log(this.group)
-        ],
-      }
+  },
+  computed: {
+    currentGroup() {
+      return this.groups[this.group - 1]
     },
 
-    computed: {
-      currentGroup() {
-        return this.groups[this.group - 1]
-      },
+    posX() {
+      return (this.toyNumber - 1) % this.currentGroup.cols
+    },
 
-      posX: {
-        get() {
-          return (this.toyNumber - 1 % this.currentGroup.cols)
-        },
-      },
-
-      posY: {
-        get() {
-          return Math.floor(this.toyNumber - 1 / this.currentGroup.cols)
-        }
-      },
-
-      avatarStyle: {
-        get() {
-          return {
-            display: "inline-block",
-            width: this.size + "px",
-            height: this.size + "px",
-            "background-image": this.currentGroup.img,
-            "background-repeat": "no-repeat",
-            "background-size":
+    posY() {
+      return Math.floor((this.toyNumber - 1) / this.currentGroup.cols)
+    },
+    avatarStyle: {
+      get() {
+        return {
+          display: "inline-block",
+          width: this.size + "px",
+          height: this.size + "px",
+          "background-image": this.currentGroup.img,
+          "background-repeat": "no-repeat",
+          "background-size":
               this.size * this.currentGroup.cols +
               "px " +
               this.size * this.currentGroup.rows +
               "px",
-            margin: "8px",
-            borderRadius: this.rounded + "px",
-            backgroundPositionX: `${this.posX * this.size}px`,
-            backgroundPositionY: `${this.posY * this.size}px`,
-          }
+          margin: "8px",
+          borderRadius: this.rounded + "px",
+          backgroundPositionX: `-${this.posX * this.size}px`,
+          backgroundPositionY: `-${this.posY * this.size}px`,
         }
       }
+    }
+  },
+  data() {
+    return {
+      groups: [
+        {
+          rows: 3,
+          cols: 6,
+          img: "url('https://user-images.githubusercontent.com/58827166/184355357-0b278997-c163-45cf-a575-19f532b07864.jpg')",
+        },
+        {
+          rows: 3,
+          cols: 6,
+          img: "url('https://user-images.githubusercontent.com/58827166/184355612-a8b12e00-a815-4456-8892-30836b4d1c2c.jpg')",
+        },
+      ],
     }
   }
 }
