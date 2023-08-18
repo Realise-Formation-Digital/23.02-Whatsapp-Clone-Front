d32cb5630e904d6f87e5ab7b74de97f20e9e9ee2
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-08-18 09:16:10 +0200

    ok

--- src/libs/router.js
+++ src/libs/router.js
@@ -3,14 +3,27 @@ import { createApp } from 'vue';
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
 import Messages from "../pages/Messages.vue";
-import {chatStore} from "../store/store.js"
+import { chatStore } from "../store/store.js"
 
 
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/messages', component: Messages },
+  { path: '/', component: Home, beforeEnter: (to, from, next) => {
+    const isUserLoggedIn = localStorage.getItem('userName')
+    if (isUserLoggedIn === '' || isUserLoggedIn === undefined || isUserLoggedIn === null) {next({name:"Login"}) }
+    else {next()}
+  }   
+ },
+  { path: '/login', name:"Login", component: Login, beforeEnter: (to, from, next) => {
+    const isUserLoggedIn = localStorage.getItem('userName')
+    if (isUserLoggedIn === '') {next({name:"Login"}) }
+    else {next()}
+  }   },
+  { path: '/messages', name: "Messages", component: Messages, beforeEnter: (to, from, next) => {
+    const isUserLoggedIn = localStorage.getItem('userName')
+    if (isUserLoggedIn === '' || isUserLoggedIn === undefined || isUserLoggedIn === null) {next({name:"Login"}) }
+    else {next()}
+  }   }
 ]
 
 const router = createRouter({
