81d1cf70658adde51b10e4b3987e5b45b044da3b
Yasm-ina <boumarafyasmina1991@gmail.com>
2023-09-26 15:29:50 +0200

    Done + in bonus-changed-graphism

--- src/components/InputMessage.vue
+++ src/components/InputMessage.vue
@@ -1,26 +1,27 @@
 <template>
-    <v-container class="bg-lime-lighten-1">
+    <v-container>
         <v-row>
             <v-col cols="11">
-                <v-textarea v-model="msgText" variant="solo" auto-grow bg-color="amber-lighten-5">
+                <v-textarea v-model="msgText" variant="solo" auto-grow bg-color="amber-lighten-5"
+                    @keydown.enter.prevent="sendMessageOnEnter">
                 </v-textarea>
             </v-col>
             <v-col cols="1">
                 <v-dialog v-model="dialog" width="auto">
                     <template v-slot:activator="{ props }">
-                        <v-btn :class="showEmojis" v-bind="props" @click="show = !show" class="pa-3 rainbow" rounded="xl"
-                            icon="mdi-emoticon-happy-outline"></v-btn>
+                        <v-btn :class="showEmojis" v-bind="props" @click="show = !show" class="pa-3" rounded="xl"
+                            icon="mdi-emoticon-happy-outline" color="amber-lighten-5"></v-btn>
                     </template>
                     <v-card>
                         <EmojiPicker v-model="msgText" @select="onSelectEmoji" />
                         <v-card-actions>
-                            <v-btn color="red" block @click="dialog = false">Revenir au message</v-btn>
+                            <v-btn block @click="dialog = false">Revenir au message</v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-dialog>
                 <br>
                 <br>
-                <v-btn class="pa-3" icon="mdi-send" @click="sendMessage()" rounded="xl" color="teal-accent-3"></v-btn>
+                <v-btn class="pa-3" icon="mdi-send" @click="sendMessage()" rounded="xl" color="amber-lighten-5"></v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -56,19 +57,20 @@ export default {
             this.msgText += emoji.i;
         },
 
-        //receuillir les informations stocké dans msgText qui correspond à la valeur de l'input
+        //emit to parent value of msgText and selected emoji
         sendMessage() {
             this.$emit('sendme', this.msgText, this.emoji)
             this.msgText = ""
+        },
+
+        sendMessageOnEnter(pressEnter) {
+            //prevent enter touch (enter = keyCode.13) to go to next line  
+            if (pressEnter.keyCode === 13 && !pressEnter.shiftKey) {
+                this.sendMessage();
+            }
         }
     }
 }
 </script>
-
-
-
-<style scoped>
-.rainbow {
-    background: linear-gradient(25deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);
-}
+<style>
 </style>
--- src/components/SingleMessage.vue
+++ src/components/SingleMessage.vue
@@ -1,29 +1,38 @@
 <template>
-  <div :class="sender == chatStore.getUserName ?
-    'd-flex justify-end px-9 my-2' :
-    'd-flex justify-start px-9 my-2'">
-    <div :class="sender == chatStore.getUserName ?
-      'border rounded-xl rounded-be-0 messageFitContent' :
-      'border rounded-xl rounded-ts-0 messageFitContent'" :style="gradientStyle">
-      <!-- sender of the messages -->
-      <h4 class="pa-3 px-9">
-        {{ sender }}
-      </h4>
-      <!-- message content -->
-      <div class="px-3">
-        {{ message }}
-      </div>
-      <!-- date -->
-      <div class="px-3 py-2 d-flex justify-end">
-        <v-chip>
-          {{ todayFormattedDate_FullDateWellLongAndFullTime(ts) }}
-        </v-chip>
-        <!-- button for messages deletions -->
-        <v-col><v-btn class="bg-orange-darken-2" @click="deleteMessageFunc(id)"> <svg-icon type="mdi"
-              :path="path"></svg-icon></v-btn></v-col>
+  <v-timeline-item>
+    <div :class="sender == chatStore.getUserName ? 'd-flex justify-end px-9 my-2' :
+      'd-flex justify-start px-9 my-2'">
+      <div :class="sender == chatStore.getUserName ? 'border rounded-xl rounded-be-0 messageFitContent' :
+        'border rounded-xl rounded-ts-0 messageFitContent'" :style="gradientStyle">
+        <!-- sender of the messages -->
+        <v-card-title class="pa-9 px-16 text-purple-darken-4" id="titleFont">
+          {{ sender }}
+        </v-card-title>
+        <!-- message content -->
+        <v-card-text>
+          <div class="px-13" id="generalFont">
+            {{ message }}
+          </div>
+        </v-card-text>
+        <v-card-text class="px-3 py-2 d-flex justify-end">
+          <!-- date -->
+          <v-row align-self="start">
+            <v-col cols="8" align="start" no-gutter>
+              <v-chip class="ma-15 pa-5" color="purple-darken-4" prepend-icon="mdi-email">
+                {{ todayFormattedDate_FullDateWellLongAndFullTime(ts) }}
+              </v-chip>
+            </v-col>
+            <v-col justify="end" cols="3" align-self="center" class="pa-7">
+              <!-- button for messages deletions -->
+              <v-btn class="bg-orange-lighten-5" @click="deleteMessageFunc(id)"><svg-icon type="mdi"
+                  :path="path"></svg-icon></v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
       </div>
     </div>
-  </div>
+  </v-timeline-item>
+
 </template>
 <script>
 import { mapStores } from "pinia";
@@ -85,11 +94,25 @@ export default {
   },
 }
 
-
-
 </script>
-<style scoped> .messageFitContent {
-   width: fit-content;
-   min-width: 300px;
- }
+<style >
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Urbanist:wght@500&display=swap');
+
+#titleFont {
+  font-family: 'Poppins', sans-serif;
+  font-size: 30px;
+  font-weight: bold
+};
+
+#generalFont {
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+  font-weight: light;
+};
+
+/* #testDegr {
+  background-color: rgb(16, 74, 55);
+    background-image: conic-gradient(#ffe683, #86f3fffb, #c9febcfb); 
+    box-shadow: -1px 1px 4px 1px #ba61358b  
+  }; */
 </style>
